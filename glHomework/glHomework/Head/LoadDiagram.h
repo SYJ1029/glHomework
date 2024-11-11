86f44e50defcd6140224b2e50f2d55c5bccc43f1
SYJ1029 <abg0986@gmail.com>
2024-11-11 15:06:31 +0900

    아직 적용중

--- glHomework/glHomework/Head/LoadDiagram.h
+++ glHomework/glHomework/Head/LoadDiagram.h
@@ -5,23 +5,16 @@
 
 MyCol mycolor = { 1.0f, 1.0f, 1.0f, 1.0f };
 GLPos Screensize = { 800, 800, 0 };
-GL_Cube* cube = new GL_Cube;
-GL_Tetrahedron* tri = new GL_Tetrahedron;
-GL_Pyramid* pyr = new GL_Pyramid;
-GL_Sphere* sphere = new GL_Sphere;
-GL_Cylinder* cyl = new GL_Cylinder;
-GL_Cylinder* cone = new GL_Cylinder;
+GL_Tri* tri = new GL_Tri();
+Rect* rect = new GL_Rect();
 
 GLUquadricObj* qobj = gluNewQuadric();
 
 
 GLvoid IsobjsProjed(bool proj) {
 	if (proj) {
-		cube->SetTranPos(SIZEMAKRO);
 		tri->SetTranPos(SIZEMAKRO);
-		pyr->SetTranPos(SIZEMAKRO);
+		rect->SetTranPos(SIZEMAKRO);
 	}
-
-
 
 }
