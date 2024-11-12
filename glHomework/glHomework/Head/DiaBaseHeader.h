d75bd4b9aa8c6a65cc8c0768df74703446ac4c78
SYJ1029 <abg0986@gmail.com>
2024-11-12 14:58:11 +0900

    DiaBaseHeader.h를 따로 만들어서 한번만 실행되게 해보고자 했으나 어림도 없다.

---
+++ glHomework/glHomework/Head/DiaBaseHeader.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "TimerProcedure.h"
+#include "ProjMethod.h"
+#include "CheckPointQue.h"
---
+++ glHomework/glHomework/Head/LineDiagram.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "DiaBaseHeader.h"
+
+
+class GL_Line {
+
+
+
+};
