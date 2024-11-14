78f68620c0eae34c4a4f001fa589d224c5f91732
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2024-11-14 20:06:56 +0900

    변경사항 저장

--- glHomework/glHomework/Head/CheckCollision.h
+++ glHomework/glHomework/Head/CheckCollision.h
@@ -59,6 +59,50 @@ GLPos GetMin(GLPos pos1, GLPos pos2) {
 	return pos2;
 }
 
+
+GLPos GetDot(GL_Rect line, float cnt) {
+	GLPos result = { 0.0f };
+	GLPos middlepos = { 0.0f, 0.0f, 0.0f };
+	GLPos prevpos = { 1.0f, 1.0f, 1.0f };
+
+	float xradius = line.pos2.x - line.pos2.x;
+	float yradius = line.pos2.y - line.pos1.y;
+
+	int judge[2] = { 0, 0 };
+
+	for (float t = 0.0f; t < 1.0f; t += 0.01f) {
+		middlepos = { line.pos1.x + xradius * t, line.pos1.y + yradius * t, 0.0f};
+
+		judge[0] = CompareyPos(cnt, middlepos.y);
+		judge[1] = CompareyPos(cnt, prevpos.y);
+
+
+		if (judge[0] != judge[1]) {
+			result = (middlepos - prevpos) / 2 + prevpos;
+			return result;
+		}
+
+		prevpos = middlepos;
+	}
+
+
+	return { -2.0f, -2.0f, -2.0f };
+}
+
+GL_Rect GetCrossDots(GL_Rect line1, GL_Rect line2, float cnt) {
+	GL_Rect result = { {0.0f}, {0.0f} };
+
+
+	result.pos1 = GetDot(line1, cnt);
+	result.pos2 = GetDot(line2, cnt);
+
+	// 중간 검사 문항 넣기
+
+
+	return result;
+}
+
+
 void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 
 	GLPos poses[5];
@@ -69,16 +113,14 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 
 	list<int> uplist;
 	list<int> bottomlist;
-
-	auto upit = uplist.begin();
-	auto bottomit = bottomlist.begin();
-
-	int count = 0;
-	int index = 0;
+	
 
 	for (int i = 0; i < 3; i++) {
 
 
+		if (playground[i].center.x >= -1.0f && playground[i].center.x <= 1.0f &&
+			playground[i].center.y >= -1.0f && playground[i].center.y <= 1.0f);
+		else continue;
 
 		switch (playground[i].postype) {
 		case ID_TRI:
@@ -90,8 +132,8 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 
 
 				poses[j] *= tri[0]->Stretch;
-				poses[j] = Spin(-1 * theta, poses[j], Opos);
 				poses[j] += playground[i].center;
+				poses[j] = Spin(-1 * theta, poses[j], line2);
 
 				//if (poses[j].x <= GetMax(line1, line2).x && poses[j].x >= GetMin(line1, line2).x);
 				//else continue;
@@ -101,9 +143,9 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 
 				upordown[j] = CompareyPos(line1.y, poses[j].y);
 
-				if (upordown[j] < 0)
-					uplist.push_back(j);
 				if (upordown[j] > 0)
+					uplist.push_back(j);
+				if (upordown[j] < 0)
 					bottomlist.push_back(j);
 
 
@@ -115,8 +157,8 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 		case ID_RECT:
 			for (int j = 0; j < 4; j++) {
 				poses[j] = {rect[0]->pos[j][0], rect[0]->pos[j][1], rect[0]->pos[j][2]};
-				poses[j] = Spin(-1 * theta, poses[j], Opos);
 				poses[j] += playground[i].center;
+				poses[j] = Spin(-1 * theta, poses[j], line2);
 
 
 				//if (poses[j].x > GetMax(line1, line2).x || poses[j].x < GetMin(line1, line2).x)
@@ -137,8 +179,8 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 			for (int j = 0; j < 5; j++) {
 				poses[j] = {pent[0]->pos[j][0], pent[0]->pos[j][1], pent[0]->pos[j][2]};
 				poses[j] *= pent[0]->Stretch;
-				poses[j] = Spin(-1 * theta, poses[j], Opos);
 				poses[j] += playground[i].center;
+				poses[j] = Spin(-1 * theta, poses[j], line2);
 
 				//if (poses[j].x > GetMax(line1, line2).x || poses[j].x < GetMin(line1, line2).x)
 				//	break;
@@ -160,37 +202,28 @@ void CheckingLoop(float theta, GLPos line1, GLPos line2) {
 		if (uplist.empty() || bottomlist.empty());
 		else {
 
-			if(playground[i].center.x >= -1.0f && playground[i].center.x <= 1.0f &&
-				playground[i].center.y >= -1.0f && playground[i].center.y <= 1.0f)
-				cout << i+1 << "번째 " << playground[i].postype + 3 << "각형 교차합니다" << endl;
+			cout << i + 1 << "번째 " << playground[i].postype + 3 << "각형 교차합니다" << endl;
 
+			GL_Rect bline;
+			GL_Rect eline;
+			GL_Rect lineRect;
+
+			bline = { poses[uplist.back()], poses[bottomlist.front()]};
+			eline = { poses[bottomlist.back()], poses[uplist.front()] };
 			
-			upit = uplist.begin();
-			bottomit = bottomlist.begin();
+			//if (bottomlist.front() == 0) {
+			//	lineRect = bline;
+			//	bline = eline;
+			//	eline = lineRect;
 
-			for (upit = uplist.begin(); *upit != uplist.size() && *bottomit != bottomlist.size(); count++) {
-
-				if (*upit == count) {
-					if (*(++bottomit) == count + 1) {
-						cout << "교차점1" << endl;
-					}
-					
-					++upit;
-				}
-
-				else if (*bottomit == count) {
-					if (*(++upit) == count + 1) {
-						cout << "교차점2" << endl;
-					}
-
-					++bottomit;
-				}
+			//}
 
 
-			}
-			
+			lineRect = GetCrossDots(bline, eline, line1.y);
 
-			count = 0;
+			cout << "(" << lineRect.pos1.x << ", " << lineRect.pos1.y << "), "
+				<< "(" << lineRect.pos2.x << ", " << lineRect.pos2.y << ")" << endl;
+			 
 		}
 
 
